eed9ca68660dc6ec17907a0d4f417d49b66a82c4
Dmitriy <fed3375546@yandex.ru>
2021-01-15 12:09:46 +0300

    Eureka Service Discovery Server

---
+++ src/test/java/ru/fed3375546/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fed3375546.PhotoAppDiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
